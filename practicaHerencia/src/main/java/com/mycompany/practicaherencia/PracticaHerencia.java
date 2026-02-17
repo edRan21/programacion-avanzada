f0914a8ff1a41d8890aa7409634d68a8ea85271d
edRan21 <165865586+edRan21@users.noreply.github.com>
2026-02-17 15:59:34 -0600

    Practica herencia
    Identificar instancias

---
+++ practicaHerencia/src/main/java/com/mycompany/practicaherencia/PracticaHerencia.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicaherencia;
+
+/**
+ *
+ * @author SALON206
+ */
+import java.util.Scanner;
+
+class Biblioteca{
+    public String nombre;
+    
+    public void escribirNombre(){
+        nombre = "Alejandro Reyes Cristiani";
+        System.out.println("Bliblioteca: "+ nombre);
+    }
+}
+
+class Libro{
+    // Atributos
+    public String titulo;
+    public String autor;
+    public String editorial;
+    public int añoPublicacion;
+    public int edicion;
+    public int ISBN;
+    
+    // Métodos leer (actuan sobre las varibles de la clase Libro)
+    public void leerTitulo(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el titulo del libro: ");
+        titulo = leer.nextLine();
+    }
+    
+    public void leerAutor(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el autor del libro: ");
+        autor = leer.nextLine();
+    }
+    
+    public void leerEditorial(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la editorial del libro: ");
+        editorial = leer.nextLine();
+    }
+    
+    public void leerAñoPublicacion(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el año de publicación del libro: ");
+        añoPublicacion = leer.nextInt();
+    }
+    
+    public void leerEdicion(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la edición del libro: ");
+        edicion = leer.nextInt();
+    }
+    
+    public void leerISBN(){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese el ISBNA del libro: ");
+        ISBN = leer.nextInt();
+    }
+    
+    // Métodos escribir
+    public void escribirTitulo(){
+        System.out.println("El Titulo es: "+ titulo);
+    }
+    
+    public void escribirAutor(){
+        System.out.println("El Autor es: "+ autor);
+    }
+    
+    public void escribirEditorial(){
+        System.out.println("La Editorial es: "+ editorial);
+    }
+    
+    public void escribirAñoPublicacion(){
+        System.out.println("El año de publicación es: "+ añoPublicacion);
+    }
+    
+    public void escribirEdicion(){
+        System.out.println("La edición es: "+ edicion);
+    }
+    
+    public void escribirISBN(){
+        System.out.println("El ISBN es: "+ ISBN);
+    }
+}
+class Tesis extends Libro{
+    public String asesor;
+    public String coAsesor;
+    public String carreras;
+    
+    public void leerAsesor(){
+        
+    }
+}
+
+public class PracticaHerencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
