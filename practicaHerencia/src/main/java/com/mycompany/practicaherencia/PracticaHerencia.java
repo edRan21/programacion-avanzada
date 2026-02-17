c8a256b2ef7056eb2dcbb374d0588035a1955e54
edRan21 <165865586+edRan21@users.noreply.github.com>
2026-02-17 15:57:59 -0600

    Practica de implementación de herencia
    Identificar declaración de instacias

---
+++ practicaHerencia/src/main/java/com/mycompany/practicaherencia/PracticaHerencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicaherencia;
+
+/**
+ *
+ * @author SALON206
+ */
+public class PracticaHerencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
